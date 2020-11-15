33969b5cf8866203a539923ef2f17c467febe821
NEMMFIELD <markov-nik@yandex.ru>
2020-11-15 20:07:11 +0300

    version 0.9

--- app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.androidacademyhomework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView:TextView=findViewById(R.id.textView_1)
+        textView.setOnClickListener { openActivityMovieDetails() }
+
+    }
+
+    private fun openActivityMovieDetails()
+    {
+        val intent= Intent(this,MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
