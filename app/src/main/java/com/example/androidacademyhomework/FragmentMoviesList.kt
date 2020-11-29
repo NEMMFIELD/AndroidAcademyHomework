d70c18769f33a9a1a67208f1f4983c110e8c87eb
NEMMFIELD <markov-nik@yandex.ru>
2020-11-29 17:44:13 +0300

    version 1.04

--- app/src/main/java/com/example/androidacademyhomework/FragmentMoviesList.kt
+++ app/src/main/java/com/example/androidacademyhomework/FragmentMoviesList.kt
@@ -1,6 +1,5 @@
 package com.example.androidacademyhomework
 
-import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +8,20 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 
-class FragmentMoviesList: Fragment()
-{
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-    {
-        val v:View=inflater.inflate(R.layout.fragment_movies_list,container,false)
-        val nextScr:ImageView=v.findViewById(R.id.movie_img)
+class FragmentMoviesList : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        val nextScr: ImageView = v.findViewById(R.id.movie_img)
         nextScr.setOnClickListener {
-            val fragment:Fragment=FragmentMoviesDetails()
-            val fragmentManager:FragmentManager= requireActivity().supportFragmentManager
-            val fragmentTransaction:FragmentTransaction=fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.main_container,fragment)
+            val fragment: Fragment = FragmentMoviesDetails()
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.main_container, fragment)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
--- app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyhomework/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.androidacademyhomework
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var nextClick:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_container,FragmentMoviesList())
-            .addToBackStack(null)
-            .commit()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, FragmentMoviesDetails())
+                .add(R.id.main_container, FragmentMoviesList())
+                .commit()
+        }
 
     }
-
-
-
-
 
 
 }
