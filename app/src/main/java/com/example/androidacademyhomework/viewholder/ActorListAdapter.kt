fbed4b3efb9374bf3dc29d3d0869cb3607da0bcb
NEMMFIELD <markov-nik@yandex.ru>
2020-12-06 19:09:09 +0300

    version 1.13 force vector assets to drawable-v24

--- app/src/main/java/com/example/androidacademyhomework/viewholder/ActorListAdapter.kt
+++ app/src/main/java/com/example/androidacademyhomework/viewholder/ActorListAdapter.kt
@@ -8,39 +8,39 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.androidacademyhomework.R
 import com.example.androidacademyhomework.model.Actor
 
-class ActorListAdapter (private var listActors:List<Actor>):RecyclerView.Adapter<ActorListAdapter.ActorListViewHolder>()
-{
-    inner class ActorListViewHolder(inflater:LayoutInflater,parent: ViewGroup):RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_actor,parent,false))
-    {
-        private var actorImage:ImageView?=null
-        private var actorName: TextView?=null
+class ActorListAdapter(private var listActors: List<Actor>) :
+    RecyclerView.Adapter<ActorListAdapter.ActorListViewHolder>() {
+    inner class ActorListViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_actor, parent, false)) {
+        private var actorImage: ImageView? = null
+        private var actorName: TextView? = null
+
         init {
 
-            actorImage=itemView.findViewById(R.id.actor_image)
-            actorName=itemView.findViewById(R.id.actor_name)
+            actorImage = itemView.findViewById(R.id.actor_image)
+            actorName = itemView.findViewById(R.id.actor_name)
         }
 
 
-        fun bind(actor:Actor)
-        {
+        fun bind(actor: Actor) {
             actorImage?.setImageResource(actor.imageActor)
-            actorName?.text=actor.name
+            actorName?.text = actor.name
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorListViewHolder {
-        val inflater=LayoutInflater.from(parent.context)
-        return ActorListViewHolder(inflater,parent)
+        val inflater = LayoutInflater.from(parent.context)
+        return ActorListViewHolder(inflater, parent)
     }
 
     override fun onBindViewHolder(holder: ActorListViewHolder, position: Int) {
-        val actors: Actor =listActors[position]
+        val actors: Actor = listActors[position]
         holder.bind(actors)
     }
 
     override fun getItemCount(): Int {
-    return listActors.size
+        return listActors.size
     }
 
 }
