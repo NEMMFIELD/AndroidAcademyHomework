ad90a3a0e144553fe780d6d9aced4a5e62d406b4
NEMMFIELD <markov-nik@yandex.ru>
2020-12-19 21:29:54 +0300

    version 1.15 Первая версия JSon Parse

--- app/src/main/java/com/example/androidacademyhomework/viewholder/MovieListAdapter.kt
+++ app/src/main/java/com/example/androidacademyhomework/viewholder/MovieListAdapter.kt
@@ -5,27 +5,38 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.androidacademyhomework.R
-import com.example.androidacademyhomework.model.Model
+import com.example.androidacademyhomework.data.Movie
 
 class MovieListAdapter(
-    private var listMovies: List<Model>,
+    private var listMovies: List<Movie>,
     private val cellClickListener: CellClickListener
-) : RecyclerView.Adapter<MovieListAdapter.MovieListViewHolder>() {
-    inner class MovieListViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-        RecyclerView.ViewHolder(inflater.inflate(R.layout.view_holder_movie, parent, false)) {
+) : RecyclerView.Adapter<MovieListAdapter.MovieListViewHolder>()
+{
+    inner class MovieListViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(
+        inflater.inflate(
+            R.layout.view_holder_movie,
+            parent,
+            false
+        )
+    )
+    {
+
         private var imageMain: ImageView? = null
         private var titleName: TextView? = null
         private var duration: TextView? = null
         private var numbReviews: TextView? = null
         private var age: ImageView? = null
-        private var genre: TextView? = null
+        var genre: TextView? = null
         private var like: ImageView? = null
         private var firstStar: ImageView? = null
         private var secondStar: ImageView? = null
         private var thirdStar: ImageView? = null
         private var fourthStar: ImageView? = null
         private var fifthStar: ImageView? = null
+
+
         init {
             imageMain = itemView.findViewById(R.id.movie_img)
             titleName = itemView.findViewById(R.id.cinema_title)
@@ -40,34 +51,38 @@ class MovieListAdapter(
             fourthStar = itemView.findViewById(R.id.redstar_4)
             fifthStar = itemView.findViewById(R.id.non_star)
         }
-        fun bind(model: Model) {
-            imageMain?.setImageResource(model.imageMain)
-            titleName?.text = model.titleName
-            duration?.text = model.duration
-            numbReviews?.text = model.numberReviews
-            age?.setImageResource(model.ageRate)
-            genre?.text = model.genre
-            like?.setImageResource(model.liked)
-            firstStar?.setImageResource(model.star1)
-            secondStar?.setImageResource(model.star2)
-            thirdStar?.setImageResource(model.star3)
-            fourthStar?.setImageResource(model.star4)
-            fifthStar?.setImageResource(model.star5)
+
+        fun bind(movie: Movie) {
+            Glide.with(itemView.context).load(listMovies[layoutPosition].poster).into(imageMain!!)
+            titleName?.text=movie.title
+            duration?.text=movie.runtime.toString()+" MIN"
+            numbReviews?.text=movie.ratings.toInt().toString()
+            val builder = StringBuilder()
+            for (n in movie.genres) {
+                builder.append(n.name+", ")
+            }
+            builder.deleteCharAt(builder.lastIndexOf(","));
+          genre?.text= builder.toString()
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return MovieListViewHolder(inflater, parent)
     }
+
     override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) {
-        val movieList: Model = listMovies[position]
+        val movieList: Movie = listMovies[position]
         holder.bind(movieList)
         holder.itemView.setOnClickListener { cellClickListener.onCellClickListener() }
     }
+
     override fun getItemCount(): Int {
         return listMovies.size
     }
+
 }
+
 interface CellClickListener {
     fun onCellClickListener()
 }
