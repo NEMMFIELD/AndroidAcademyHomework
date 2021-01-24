fb889d0521326483ff23c76606af657b502e98ea
NEMM <52427720+NEMMFIELD@users.noreply.github.com>
2021-01-24 21:06:12 +0300

    remaster
    remaster conflict

---
+++ app/src/main/java/com/example/androidacademyhomework/FragmentMoviesList.kt
@@ -0,0 +1,67 @@
+package com.example.androidacademyhomework
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidacademyhomework.data.loadMovies
+import com.example.androidacademyhomework.viewholder.CellClickListener
+import com.example.androidacademyhomework.viewholder.MovieListAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class FragmentMoviesList : Fragment(), CellClickListener {
+    private var movieList_recycler: RecyclerView? = null
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        // val nextScr: ImageView = v.findViewById(R.id.test_click)
+        // nextScr.setOnClickListener { view ->
+        //   Navigation.findNavController(view).navigate(R.id.navigateToSecond)
+        // }
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        scope.launch {
+            super.onViewCreated(view, savedInstanceState)
+            movieList_recycler = view.findViewById(R.id.list_recycler_view)
+            movieList_recycler?.apply {
+                layoutManager = GridLayoutManager(activity, 2)
+                movieList_recycler?.setHasFixedSize(true)
+                adapter = MovieListAdapter(loadMovies(requireContext()), this@FragmentMoviesList)
+               // adapter?.setHasStableIds(true)
+
+            }
+        }
+    }
+
+    override fun onCellClickListener(view: View, position: Int) {
+        val fragment: Fragment = FragmentMoviesDetails()
+        val bundle=Bundle()
+        bundle.putInt("pos",position)
+        fragment.arguments=bundle
+        val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/androidacademyhomework/viewholder/MovieListAdapter.kt
@@ -0,0 +1,94 @@
+package com.example.androidacademyhomework.viewholder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.androidacademyhomework.R
+import com.example.androidacademyhomework.data.Movie
+
+
+class MovieListAdapter(
+    private var listMovies: List<Movie>,
+    private val cellClickListener: CellClickListener
+) : RecyclerView.Adapter<MovieListAdapter.MovieListViewHolder>() {
+    inner class MovieListViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(
+            inflater.inflate(
+                R.layout.view_holder_movie,
+                parent,
+                false
+            )
+        ) {
+        private var imageMain: ImageView? = null
+        private var titleName: TextView? = null
+        private var duration: TextView? = null
+        private var numbReviews: TextView? = null
+        private var age: ImageView? = null
+        var genre: TextView? = null
+        private var like: ImageView? = null
+        private var firstStar: ImageView? = null
+        private var secondStar: ImageView? = null
+        private var thirdStar: ImageView? = null
+        private var fourthStar: ImageView? = null
+        private var fifthStar: ImageView? = null
+
+        init {
+            imageMain = itemView.findViewById(R.id.movie_img)
+            titleName = itemView.findViewById(R.id.cinema_title)
+            duration = itemView.findViewById(R.id.duration)
+            numbReviews = itemView.findViewById(R.id.name)
+            age = itemView.findViewById(R.id.ageRate)
+            genre = itemView.findViewById(R.id.tag)
+            like = itemView.findViewById(R.id.toLike)
+            firstStar = itemView.findViewById(R.id.redstar_1)
+            secondStar = itemView.findViewById(R.id.redstar_2)
+            thirdStar = itemView.findViewById(R.id.redstar_3)
+            fourthStar = itemView.findViewById(R.id.redstar_4)
+            fifthStar = itemView.findViewById(R.id.non_star)
+        }
+
+        fun bind(movie: Movie) {
+            val builder_MIN = StringBuilder()
+            Glide.with(itemView.context).load(listMovies[layoutPosition].poster).into(imageMain!!)
+            titleName?.text = movie.title
+            builder_MIN.append(movie.runtime.toString() + " MIN")
+            duration?.text=builder_MIN
+            val builder_REVIEWS=StringBuilder()
+            builder_REVIEWS.append(movie.numberOfRatings.toString()+" REVIEWS")
+            numbReviews?.text = builder_REVIEWS
+            val builder = StringBuilder()
+            for (n in movie.genres) {
+                builder.append(n.name + ", ")
+            }
+            builder.deleteCharAt(builder.lastIndexOf(","));
+            genre?.text = builder.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MovieListViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) {
+        val movieList: Movie = listMovies[position]
+        holder.bind(movieList)
+        val item = listMovies.get(holder.adapterPosition)
+        holder.itemView.setOnClickListener {
+            cellClickListener.onCellClickListener(holder.itemView,position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listMovies.size
+    }
+}
+
+interface CellClickListener {
+    fun onCellClickListener(view: View, position: Int)
+}
+
