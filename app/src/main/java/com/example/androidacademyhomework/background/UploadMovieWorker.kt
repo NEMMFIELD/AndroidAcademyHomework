332ac88d11d200958c90f832987c0e33403b5a97
NEMMFIELD <markov-nik@yandex.ru>
2021-10-10 21:13:46 +0300

    WorkManager

--- app/src/main/java/com/example/androidacademyhomework/background/UploadMovieWorker.kt
+++ app/src/main/java/com/example/androidacademyhomework/background/UploadMovieWorker.kt
@@ -1,6 +1,7 @@
 package com.example.androidacademyhomework.background
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.asLiveData
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
@@ -9,13 +10,17 @@ import com.example.androidacademyhomework.database.MovieEntity
 import com.example.androidacademyhomework.network.MovieRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.ExperimentalSerializationApi
 
-class UploadMovieWorker(context:Context,workerParameters: WorkerParameters): CoroutineWorker(context,workerParameters) {
+@ExperimentalSerializationApi
+class UploadMovieWorker(context:Context, workerParameters: WorkerParameters): CoroutineWorker(context,workerParameters) {
     override suspend fun doWork(): Result {
         return withContext(Dispatchers.IO) {
             try{
                 val repository = MovieRepo(context = applicationContext)
-                writeToDb(movies = repository.allMovies.asLiveData().value!!)
+                repository.allMovies.asLiveData().value?.let { writeToDb(movies = it) }
+                //repository.addNewAndGetUpdated() 2 вариант
+                Log.d("do work success","doWork: Success function called")
                  Result.success()
             }
             catch (e:Exception)
--- app/src/main/java/com/example/androidacademyhomework/background/WorkRepository.kt
+++ app/src/main/java/com/example/androidacademyhomework/background/WorkRepository.kt
@@ -4,11 +4,13 @@ package com.example.androidacademyhomework.background
 import androidx.work.*
 import com.example.androidacademyhomework.Utils.Companion.WORKER_DELAY_TIME
 import com.example.androidacademyhomework.Utils.Companion.WORKER_REPEAT_TIME
+import kotlinx.serialization.ExperimentalSerializationApi
 import java.util.concurrent.TimeUnit
 
 class WorkRepository {
     private val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.UNMETERED)
         .setRequiresCharging(true).build()
+    @ExperimentalSerializationApi
     val periodicWork = PeriodicWorkRequest.Builder(
         UploadMovieWorker::class.java,
         WORKER_REPEAT_TIME,
