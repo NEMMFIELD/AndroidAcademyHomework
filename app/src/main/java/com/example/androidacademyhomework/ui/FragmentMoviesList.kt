3bf1fe4df31f77a82296ab1da84d1886fe22ce4d
NEMMFIELD <markov-nik@yandex.ru>
2021-12-15 22:56:36 +0300

    1

--- app/src/main/java/com/example/androidacademyhomework/ui/FragmentMoviesList.kt
+++ app/src/main/java/com/example/androidacademyhomework/ui/FragmentMoviesList.kt
@@ -6,11 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -31,7 +30,7 @@ class FragmentMoviesList : Fragment() {
     private var _binding: FragmentMoviesListBinding? = null
     private val binding get() = _binding
     private val appContainer = MyApp.container
-    private lateinit var viewModelShared: SharedViewModel
+
     private val workRepository = WorkRepository()
 
     @ExperimentalSerializationApi
@@ -52,14 +51,14 @@ class FragmentMoviesList : Fragment() {
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModelShared = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
-        viewModelShared.bundleFromFragmentBToFragmentA.observe(viewLifecycleOwner, Observer {
+       // viewModelShared = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+       /* viewModelShared.bundleFromFragmentBToFragmentA.observe(viewLifecycleOwner, Observer {
             val message = it.getString("ARGUMENT_MESSAGE", "")
             Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show()
-        })
+        })*/
         // viewModel = ViewModelProvider(this, MovieViewModelFactory((requireActivity() as MainActivity).repository)).get(MovieViewModel::class.java)
         movieListRecycler = binding?.listRecyclerView
-        val layoutManager: GridLayoutManager = GridLayoutManager(context, 2)
+        val layoutManager = GridLayoutManager(context, 2)
         movieListRecycler?.layoutManager = layoutManager
         adapter = MovieListAdapter(
             clickListener = listener
